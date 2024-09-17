b87081dbdcb54946367c9e0450186384d77afefc
Ifram Lopes <160543554+IfraimLopes@users.noreply.github.com>
2024-09-17 10:58:33 +0100

    upgrade

---
+++ script.js
@@ -0,0 +1,19 @@
+function click1() {
+  let imagem_mae = document.getElementById("img1");
+  imagem_mae.src = "images/Samba_OG_Shoes_Green_IE0872_01_standard.png";
+}
+
+function click2() {
+  let imagem_mae = document.getElementById("img1");
+  imagem_mae.src = "images/Samba_OG_Shoes_Green_IE0872_06_standard.png";
+}
+
+function click3() {
+  let imagem_mae = document.getElementById("img1");
+  imagem_mae.src = "images/Samba_OG_Shoes_Green_IE0872_41_detail.png";
+}
+
+function click4() {
+  let imagem_mae = document.getElementById("img1");
+  imagem_mae.src = "images/Samba_OG_Shoes_Green_IE0872_42_detail.png";
+}
