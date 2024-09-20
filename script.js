28a9a5ae74151a2ae9f973d442c0548cf325fde4
Ifram Lopes <160543554+IfraimLopes@users.noreply.github.com>
2024-09-20 19:01:12 +0100

    insert

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+/*Funções da Troca de Imagens da área do "Comprar"*/
+
 function click1() {
   let imagem_mae = document.getElementById("img1");
   imagem_mae.src = "images/Samba_OG_Shoes_Green_IE0872_01_standard.png";
@@ -16,4 +18,28 @@ function click3() {
 function click4() {
   let imagem_mae = document.getElementById("img1");
   imagem_mae.src = "images/Samba_OG_Shoes_Green_IE0872_42_detail.png";
+}
+
+/*Funcoes de Alertas*/
+
+function shopping() {
+  window.alert("Carrinho indisponível");
+}
+
+function user() {
+  window.alert("Função Indisponível, lamentamos.");
+}
+
+/*Script do Dialog*/
+let button = document.getElementById("botao1");
+let bu = document.getElementById("ok");
+let modal = document.querySelector("dialog");
+
+button.onclick = function () {
+  modal.showModal();
+};
+
+function ok() {
+  modal.close();
+  window.alert("Login feito com Sucesso!!");
 }
